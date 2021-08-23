e108b70ced3b596b3f9be935f7a6b25f22299b71
hosseinsho <amiramiry125@gmail.com>
2021-08-23 18:03:46 +0500

    Telegram UI changed and keyboard added instead of commands

--- traderbot/settings.py
+++ traderbot/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-APP_VERSION = "0.6.2"
+APP_VERSION = "0.7.0"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', "afxaafawb82g638wgq87g7grayrby6468q2918+")
